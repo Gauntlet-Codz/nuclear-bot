1a60d6673738d5cab640708f5ed9397f95baeb0f
Gauntlet-Codz <36602844+Gauntlet-Codz@users.noreply.github.com>
2018-02-18 19:18:11 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,82 @@
+const Discord = require("discord.js");
+const commando = require("discord.js-commando");
+const TOKEN = "NDE0ODMyNjgwMjMyODEyNTQ0.DWtHoA.hsBRUTE40IgDD93ByATF3bvooFg";
+const PREFIX = ":";
+const YTDL = require("ytdl-core");
+
+
+
+function play(connection, message) {
+    var server= servers[message.guild.id];
+
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+
+    server.queue.shift();
+
+    server.dispatcher.on("end", function() {
+    if (server.queue[0]) play(connection, message);
+    else connection.disconnect();
+    }
+    )};
+
+var bot = new Discord.Client();
+
+var servers = {};
+
+bot.on("ready", function(){
+console.log("Ready")
+});
+
+
+bot.on("message", function(message) {
+console.log(message.content);
+
+if (message.author.equals(bot.user)) require;
+
+if (message.content.startsWith(PREFIX)) require;
+
+var args = message.content.substring(PREFIX.length).split(" ");
+
+switch (args[0].toLowerCase()) {
+case "rules":
+message.channel.sendMessage("1. No name calling. 2. NEVER remove face protection. 3. Respect field host. 4. Call your hits.");
+break;
+case "gamemodes":
+message.channel.sendMessage("1. TDM 2. Hostage 3. FFA 4. CTF")
+break;
+case "about":
+message.channel.sendMessage("Creator: Gauntlet")
+break;
+case "skip":
+var server = servers[message.guild.id];
+
+if (server.dispatcher) server.dispatcher.end();
+break;
+case "stop":
+var server = servers[message.guild.id];
+
+if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+break;
+case "play":
+    if (!args[1]) {
+message.channel.sendMessage("Please provide a link");
+return;
+}
+
+if (!message.member.voiceChannel) {
+    message.channel.sendMessage("You must be in a voice channel");
+    return;
+}
+
+var server = servers[message.guild.id];
+
+server.queue.push(args[1]);
+
+if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+   play(connection, message);
+});
+
+}
+});
+
+bot.login(process.env.BOT_TOKEN);
